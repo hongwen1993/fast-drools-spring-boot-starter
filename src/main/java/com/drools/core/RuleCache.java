eba8411e387891aecab9a865ea829efa7d3d2f94
hongw <Hhh09280511>
2020-02-10 11:41:16 +0800

    更新一个测试版

--- src/main/java/com/drools/core/RuleCache.java
+++ src/main/java/com/drools/core/RuleCache.java
@@ -2,7 +2,7 @@ package com.drools.core;
 
 /**
  * @author <a href="mailto:hongwen0928@outlook.com">Karas</a>
- * @date 2019/9/28 13:53
+ * @date 2019/9/28
  * @since 1.0.0
  */
 public class RuleCache implements Runnable{
