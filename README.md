af833b952ee553bb74ee8622efbcee93016ccf62
hongwen <15700051127@163.com>
2020-03-17 21:50:26 +0800

    支持classpath资源目录格式

--- README.md
+++ README.md
@@ -1,34 +1,34 @@
-![Milvuslogo](https://github.com/hongwen1993/fast-drools-spring-boot-starter/blob/master/logo.png)
+![FastDroolsLogo](https://github.com/hongwen1993/fast-drools-spring-boot-starter/blob/master/logo.png)
 
 ![LICENSE](https://img.shields.io/badge/license-Apache--2.0-brightgreen)
 ![Language](https://img.shields.io/badge/language-JAVA-blue)
 
 
 ## 介绍
-`fast-drools-spring-boot-starter`提供了SpringBoot与Drools的快速整合，并且能够无感知地实现规则文件的动态加载，
-方便开发人员以及运维人员。
+`fast-drools-spring-boot-starter` 将 SpringBoot 与规则引擎 Drools 完美融合，提供了更高效，更便捷地访问方式，同时无感知地实现规则文件的动态更新，适用于生产环境的热部署。
 
 
 ## 讨论
+有问必答，上到宇宙洪荒，下到八字良缘。更期待您的意见和建议，不定期更新各种技术栈讲解视频。
+
 - QQ群：476067618
 - Drools视频教程：https://space.bilibili.com/564757
 
-
-
 ## 使用方法
-- 1）在`pom.xml`中引入依赖（update 2020/03/10）：
+- 1）在`pom.xml`中引入依赖（**update 2020/03/17**）：
 
   ```xml
   <dependency>
       <groupId>com.github.hongwen1993</groupId>
       <artifactId>fast-drools-spring-boot-starter</artifactId>
-      <version>2.1.0</version>
+      <version>2.1.6</version>
   </dependency>
   ```
 - 2）在配置文件中指定规则文件的路径
 
   ```xml
   # 指定规则文件文件夹，会自动扫描该目录下所有规则文件，决策表，以及CSV文件
+  # 支持classpath资源目录，如：classpath:drools/**/*.drl
   spring.drools.path = C:\\DRL\\
   # 也可以指定全局的mode，选择stream或cloud
   spring.drools.mode = stream
@@ -50,30 +50,25 @@
 
 （KieTemplate 下封装了许多 Drools 的功能，许多便捷的 API 等你来发现！）
 
-
-
-
-
 ## 当前功能
 
-- SpringBoot与Drools快速整合，再也不需要配置繁琐的kmodule.xml啦。
+- SpringBoot 与 Drools 快速整合，再也不需要配置繁琐的 kmodule.xml 啦。
 - 规则文件动态加载
 - 规则文件分组控制
 - 使用NIO的文件映射，更快速的文件的读写
 - 基于缓存的规则文件控制，更高效的规则评估
-
-
+- 支持各种路径格式
 
 
 ## 未来功能
+
+- 选择性记录日志输出
+- 规则执行监控
 - 基于分布式的评估与决策
+- 规则执行成功失败率统计
 - 规则文件的GUI控制
 - 决策表的GUI控制
 - 分布式规则文件的存储
-
-
-
-
 
 
 ## 开源协议
--- src/main/java/com/drools/core/util/FileUtil.java
+++ src/main/java/com/drools/core/util/FileUtil.java
@@ -1,34 +1,60 @@
 package com.drools.core.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 /**
  * @author <a href="mailto:hongwen0928@outlook.com">Karas</a>
- * @date 2019/9/27 14:26
+ * @date 2019/9/27
  * @since 1.0.0
  */
 public class FileUtil {
 
-    public static List<File> fileList(String path, List<File> fileList) {
-        File file = new File(path);
-        if (file.exists()) {
-            if (!file.isDirectory()) {
-                // base case
-                fileList.add(file);
+    private static Logger LOG = LoggerFactory.getLogger(FileUtil.class);
+
+    /**
+     * 整合所有文件，到所传的List中
+     *
+     * @param path      可能含有classpath形式，因此需要进行判断
+     * @param fileList  {@code List<File>} 所传入的FileList
+     */
+    public static void fileList(String path, List<File> fileList) {
+        if (path.startsWith(ResourceLoader.CLASSPATH_URL_PREFIX)) {
+            try {
+                ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+                Resource[] resources = resolver.getResources(path);
+                for (Resource resource : resources) {
+                    fileList.add(resource.getFile());
+                }
+            } catch (IOException e) {
+                LOG.error("资源读取异常！", e);
             }
-            if (file.isDirectory()) {
-                File[] f = file.listFiles();
-                if (f != null) {
-                    for (File nextFile : f) {
-                        fileList(nextFile.getPath(), fileList);
+        } else {
+            File file = new File(path);
+            if (file.exists()) {
+                if (!file.isDirectory()) {
+                    // base case
+                    fileList.add(file);
+                }
+                if (file.isDirectory()) {
+                    File[] f = file.listFiles();
+                    if (f != null) {
+                        for (File nextFile : f) {
+                            fileList(nextFile.getPath(), fileList);
+                        }
                     }
                 }
             }
         }
-        return fileList;
     }
-
 
 
 }
