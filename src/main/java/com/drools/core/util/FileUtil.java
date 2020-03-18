74eba1c0478c227b1271700561c3f32be6cdeef0
hongwen <15700051127@163.com>
2020-03-18 20:52:01 +0800

    修复打包后资源目录可能读取异常的问题

--- src/main/java/com/drools/core/util/FileUtil.java
+++ src/main/java/com/drools/core/util/FileUtil.java
@@ -7,8 +7,7 @@ import org.springframework.core.io.ResourceLoader;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.List;
 
 /**
@@ -21,18 +20,24 @@ public class FileUtil {
     private static Logger LOG = LoggerFactory.getLogger(FileUtil.class);
 
     /**
+     * 临时存放规则文件的文件夹
+     */
+    private static final String TEMP_DIR = "temp";
+
+    /**
      * 整合所有文件，到所传的List中
      *
-     * @param path      可能含有classpath形式，因此需要进行判断
-     * @param fileList  {@code List<File>} 所传入的FileList
+     * @param path     可能含有classpath形式，因此需要进行判断
+     * @param fileList {@code List<File>} 所传入的FileList
      */
     public static void fileList(String path, List<File> fileList) {
         if (path.startsWith(ResourceLoader.CLASSPATH_URL_PREFIX)) {
             try {
+                createTempDir();
                 ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
                 Resource[] resources = resolver.getResources(path);
                 for (Resource resource : resources) {
-                    fileList.add(resource.getFile());
+                    fileList.add(copyResourceToTempFile(resource));
                 }
             } catch (IOException e) {
                 LOG.error("资源读取异常！", e);
@@ -53,6 +58,47 @@ public class FileUtil {
                     }
                 }
             }
+        }
+    }
+
+    /**
+     * 将资源文件转为临时文件
+     *
+     * @param resource 资源目录下的文件，需要copy出来，因为打包后可能导致资源路径不正确。
+     * @return {@code File} 资源目录对应的文件，但不是实际文件，是拷贝的。
+     */
+    public static File copyResourceToTempFile(Resource resource) {
+        InputStream is = null;
+        OutputStream os = null;
+        File targetFile = null;
+        try {
+            is = resource.getInputStream();
+            byte[] buffer = new byte[is.available()];
+            is.read(buffer);
+            targetFile = new File(TEMP_DIR + File.separator + resource.getFilename());
+            os = new FileOutputStream(targetFile);
+            os.write(buffer);
+        } catch (Exception e) {
+            LOG.error("资源整合失败", e);
+        } finally {
+            try {
+                if (is != null) {
+                    is.close();
+                }
+                if (os != null) {
+                    os.close();
+                }
+            } catch (Exception e) {
+                LOG.error("无法关闭文件", e);
+            }
+        }
+        return targetFile;
+    }
+
+    public static void createTempDir() {
+        File file = new File(TEMP_DIR);
+        if (!file.exists()) {
+            file.mkdir();
         }
     }
 
